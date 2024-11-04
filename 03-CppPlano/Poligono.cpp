13d0199417ca0a199f22128abb84464938e7edaa
blaballos <laballosbrandon@gmail.com>
2024-11-04 15:25:31 -0300

    Add: function declaration´s

--- 03-CppPlano/Poligono.cpp
+++ 03-CppPlano/Poligono.cpp
@@ -7,6 +7,12 @@ unsigned const MAX_LADOS = 100;
 struct Punto { double x, y; };
 struct Poligono { array<Punto, MAX_LADOS> puntos; unsigned lados; };
 
+void addPoint(Poligono&, Punto, unsigned);
+void removePoint(Poligono&, unsigned);
+void setPoint(Poligono&, Punto, unsigned);
+unsigned getLados(const Poligono&);
+Punto getPoint(const Poligono&, unsigned);
+
 int main() {
 
 }
