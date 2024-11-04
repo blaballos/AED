d9efeb3ed9deb95f3259c0e62b2901d94d333460
blaballos <laballosbrandon@gmail.com>
2024-11-04 16:58:19 -0300

    Add: asserts test´t in main

--- 03-CppPlano/Poligono.cpp
+++ 03-CppPlano/Poligono.cpp
@@ -2,33 +2,45 @@
 #include<cassert>
 
 using std::array;
-unsigned const MAX_LADOS = 100;
+unsigned const MAX = 10;
 
 struct Punto { double x, y; };
-struct Poligono { array<Punto, MAX_LADOS> puntos; unsigned lados; };
+struct Poligono { unsigned lados; array<Punto, MAX> puntos; };
 
 void addPoint(Poligono&, Punto, unsigned);
 void removePoint(Poligono&, unsigned);
 void setPoint(Poligono&, Punto, unsigned);
-unsigned getLados(const Poligono&);
-Punto getPoint(const Poligono&, unsigned);
+unsigned getLados(Poligono);
+Punto getPoint(Poligono, unsigned);
 
 int main() {
+    Poligono polig {4, {{{0, 0}, {1, 1}, {2, 2}, {3, 3}}}};
 
+    removePoint(polig, 2);
+    assert(getLados(polig) == 3);
+    assert(getPoint(polig, 2).x == 3);
+    assert(getPoint(polig, 2).y == 3);
+
+    addPoint(polig, {2, 2}, 2);
+    assert(getLados(polig) == 4);
+    assert(getPoint(polig, 2).x == 2);
+    assert(getPoint(polig, 2).y == 2);
+
+    setPoint(polig, {4, 4}, 2);
+    assert(getPoint(polig, 2).x == 4);
+    assert(getPoint(polig, 2).y == 4);
 }
 
 void addPoint(Poligono& polig, Punto point, unsigned index) {
-    assert(index < MAX_LADOS);
     polig.lados++;
 
-    for (int i = polig.lados; i > index; i--) {
+    for (int i = polig.lados - 1; i >= index; i--) {
         polig.puntos.at(i) = polig.puntos.at(i - 1);
     }
+    polig.puntos.at(index) = point;
 }
 
 void removePoint(Poligono& polig, unsigned index) {
-    assert(index < MAX_LADOS);
-    
     for (int i = index; i < polig.lados; i++) {
         polig.puntos.at(i) = polig.puntos.at(i + 1);
     }
@@ -36,15 +48,13 @@ void removePoint(Poligono& polig, unsigned index) {
 }
 
 void setPoint(Poligono& polig, Punto point, unsigned index) {
-    assert(index < MAX_LADOS);
     polig.puntos.at(index) = point;
 }
 
-unsigned getLados(const Poligono& polig) {
+unsigned getLados(Poligono polig) {
     return polig.lados;
 }
 
-Punto getPoint(const Poligono& polig, unsigned index) {
-    assert(index < MAX_LADOS);
+Punto getPoint(Poligono polig, unsigned index) {
     return polig.puntos.at(index);
 }
